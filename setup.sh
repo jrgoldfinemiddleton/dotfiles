9f1adcead930e9e22f70c150c9c2638f2abfc6bc
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2019-09-30 20:36:37 -0700

    Add Bash configuration
    * Provide common and OS-specific configurations for Bash
    * Enable bashrc to source the configurations

--- setup.sh
+++ setup.sh
@@ -1,14 +1,33 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if ! type stow > /dev/null;
-then
+if ! type stow > /dev/null; then
   echo "Please install the 'stow' command and run this script again."
   exit 1
 fi
 
-echo 'Linking vim configuration'
+git submodule update --init --recursive
+
+echo "Linking Vim configuration"
 stow vim
-echo 'Linking git configuration'
+echo "Linking Git configuration"
 stow git
-echo 'Linking tmux configuration'
+echo "Linking Tmux configuration"
 stow tmux
+echo "Linking Bash configuration"
+stow bash
+
+# append OS-generic functions, aliases, etc to bashrc
+if ! $(grep -q "^source $HOME/.bashrc.common" > /dev/null); then
+  echo "source $HOME/.bashrc.common" >> $HOME/.bashrc
+fi
+
+# append OS-specific functions, aliases, etc to bashrc
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  if ! $(grep -q "^source $HOME/.bashrc.linux" > /dev/null); then
+    echo "source $HOME/.bashrc.linux" >> $HOME/.bashrc
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! $(grep -q "^source $HOME/.bashrc.macos" > /dev/null); then
+    echo "source $HOME/.bashrc.macos" >> $HOME/.bashrc
+  fi
+fi 
