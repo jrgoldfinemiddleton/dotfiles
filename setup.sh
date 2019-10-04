9baad5db0ae253812eb53902d4ca27522453f444
Jason Goldfine-Middleton <jrgoldfinemiddleton@gmail.com>
2019-10-03 18:32:16 -0700

    Add safe quotes to setup script

--- setup.sh
+++ setup.sh
@@ -17,17 +17,17 @@ echo "Linking Bash configuration"
 stow bash
 
 # append OS-generic functions, aliases, etc to bashrc
-if ! $(grep -q "^source $HOME/.bashrc.common" > /dev/null); then
-  echo "source $HOME/.bashrc.common" >> $HOME/.bashrc
+if ! $(grep -q "^source '$HOME/.bashrc.common'" > /dev/null); then
+  echo "source '$HOME/.bashrc.common'" >> "$HOME/.bashrc"
 fi
 
 # append OS-specific functions, aliases, etc to bashrc
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  if ! $(grep -q "^source $HOME/.bashrc.linux" > /dev/null); then
-    echo "source $HOME/.bashrc.linux" >> $HOME/.bashrc
+  if ! $(grep -q "^source '$HOME/.bashrc.linux'" > /dev/null); then
+    echo "source '$HOME/.bashrc.linux'" >> "$HOME/.bashrc"
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  if ! $(grep -q "^source $HOME/.bashrc.macos" > /dev/null); then
-    echo "source $HOME/.bashrc.macos" >> $HOME/.bashrc
+  if ! $(grep -q "^source '$HOME/.bashrc.macos'" > /dev/null); then
+    echo "source '$HOME/.bashrc.macos'" >> "$HOME/.bashrc"
   fi
 fi 
